f70f6ba4aa365415e1f331cd1ddbfa8fde035a7e
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-16-Work.local>
2026-02-08 12:11:38 +0400

    zhr

--- core/views.py
+++ core/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from core.models import *
+from core.forms import ContactForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+        
     partners = Partner.objects.order_by("-id")
     blogs = Blog.objects.order_by("-id")
+    form = ContactForm()
     context = {
         'partners': partners,
         'blogs': blogs,
+        'form': form
     }
     return render(request, 'index.html', context)
