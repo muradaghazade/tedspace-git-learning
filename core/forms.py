b6e8400da189bbf1600536dfe3ce5ac5737fb905
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-16-Work.local>
2026-02-08 12:50:50 +0400

    contact almost finished

--- core/forms.py
+++ core/forms.py
@@ -5,3 +5,12 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
+        widgets = {
+            "first_name": forms.TextInput(attrs={"class": "contact-input"}),
+            "last_name": forms.TextInput(attrs={"class": "contact-input1"}),
+            "email": forms.EmailInput(attrs={"class": "contact-input2"}),
+            "position": forms.TextInput(attrs={"class": "contact-input2"}),
+            "company": forms.TextInput(attrs={"class": "contact-input2"}),
+            "country": forms.TextInput(attrs={"class": "contact-input2"}),
+            "message": forms.Textarea(attrs={"class": "contact-text"}),
+        }
